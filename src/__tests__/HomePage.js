67df11d73afff8985c984f579d394d8c6fde7c75
Savelieva <krasilnikova.katerina@gmail.com>
2023-03-18 22:37:38 +0200

    small changes in test

--- src/__tests__/HomePage.js
+++ src/__tests__/HomePage.js
@@ -1,41 +1,37 @@
 import * as React from 'react'
-// import {rest} from 'msw'
-// import {setupServer} from 'msw/node'
-import {render, screen, waitFor} from '@testing-library/react'
+import {render, screen} from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import { HomePage } from '../pages/HomePage'
 import { COURSES } from '../__test_data__/courses.data'
 import { Provider } from 'react-redux'
 // import { AppConfig } from "../config";
-import axios, { AxiosError } from "axios";
+import axios from "axios";
 import { setupStore } from '../store/store';
 import { MemoryRouter } from 'react-router-dom'
-
-const courses = COURSES.courses.slice(0, 3)
 
 // const url = `https://${AppConfig.API_HOST}/api/v1/core/preview-courses`
 
 jest.mock('axios')
 
-const response = {
-  data: {
-    courses: courses
-  }
-} 
+const homePageComponent = <>
+  <MemoryRouter>
+    <Provider store={setupStore()}>
+      <HomePage />
+    </Provider>
+  </MemoryRouter>
+</>
 
 describe('Home Page', () => {
   
-  test('load courses on HomePage', async () => {
+  test('Render 3 courses on HomePage', async () => {
 
-    axios.get.mockReturnValue(response)
+    axios.get.mockReturnValue({
+      data: {
+        courses: COURSES.courses.slice(0, 3)
+      }
+    })
     
-    render(<>
-        <MemoryRouter>
-          <Provider store={setupStore()}>
-            <HomePage />
-          </Provider>
-        </MemoryRouter>
-      </>)
+    render(homePageComponent)
 
     const courses = await screen.findAllByTestId('course-card')
 
